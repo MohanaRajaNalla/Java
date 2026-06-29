a8226c797f4278f57ebd350b539918d8f20f65b5
MohanaRajaNalla <nallamohana623@gmail.com>
2026-06-29 19:27:44 +0530

    Data Types Java Project

---
+++ src/datatypes/PrimitiveDataTypes.java
@@ -0,0 +1,33 @@
+package datatypes;
+
+public class PrimitiveDataTypes {
+	public static void main(String[] args) {
+		byte age = 90;
+		short salary = 15000;
+		int mobileNumber = 123458782;
+		long id = 5542248334123510264L;
+		
+		float cgpa = 8.367f;
+		double exactPercentage = 74.236458;
+		
+		
+		char grade = 'A';
+		boolean isemployed = true;
+		
+		
+		System.out.println(age);
+		System.out.println(salary);
+		System.out.println(mobileNumber);
+		System.out.println(id);
+		System.out.println(cgpa);
+		System.out.println(exactPercentage);
+		System.out.println(grade);
+		System.out.println(isemployed);
+		
+		
+		
+	
+		
+	}
+
+}
