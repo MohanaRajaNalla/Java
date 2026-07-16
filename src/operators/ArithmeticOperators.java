3a82f0185e877d347e09714a304aa63ad108a725
MohanaRajaNalla <nallamohana623@gmail.com>
2026-07-16 18:56:56 +0530

    Added Arithmetic Operators

---
+++ src/operators/ArithmeticOperators.java
@@ -0,0 +1,45 @@
+package operators;
+
+public class ArithmeticOperators {
+	public static void main(String args[]) {
+		
+		int a = 10;
+		int b = 20;
+		
+		float c = 5.0f;
+		float d = 2.0f;
+		
+		
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a/b);
+		System.out.println(a %b );
+		
+		
+		System.out.println( c / d);
+		System.out.println(c + d);
+		System.out.println( c + a);
+		
+		System.out.println( a % c);
+		
+		System.out.println("a" + a);
+		System.out.println(a + d + "a" + c);
+		
+		System.out.println(c + "d");
+		System.out.println( c + d + a +d + 10);
+		
+		c = 10F;
+		System.out.println(c);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+
+}
