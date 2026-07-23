f3d2d73aae732fde87a793c04421dee03223ebce
MohanaRajaNalla <nallamohana623@gmail.com>
2026-07-23 14:43:40 +0530

    Added Releational Operators

---
+++ src/operators/relational/RealationalOperators.java
@@ -0,0 +1,36 @@
+package operators.relational;
+
+public class RealationalOperators {
+	
+	public static void main(String args[]) {
+		
+		int a = 10;
+		int b = 20;
+		 
+		int c = 10;
+		
+		System.out.println(a > b); //False
+		System.out.println(a  < b); //True
+		;
+		System.out.println(b > a);  //True
+		System.out.println(b < a); //False;
+		
+		System.out.println(a <= b); //True
+		System.out.println(a >= b); //False
+		
+		System.out.println(b >= a);  //True
+		System.out.println(b <= a ); //False
+		
+		System.out.println( a == b); //False
+		System.out.println(a == c);  //True
+		System.out.println( a!= b); //True
+		System.out.println( a!= c); //False
+		
+		
+		boolean d = a > b; //False
+		boolean f = a == b; //False
+		System.out.println(d);
+		System.out.println(f);
+	}
+
+}
