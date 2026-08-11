e889daccf0253f3fe01af6e488c71f4372180b1c
MohanaRajaNalla <nallamohana623@gmail.com>
2026-08-11 16:35:37 +0530

    Added Pattern Problems

---
+++ src/com/patterns/Pattern2.java
@@ -0,0 +1,26 @@
+package com.patterns;
+
+public class Pattern2 {
+	public static void main(String[] args) {
+		
+		
+		
+	for(int k = 5; k >= 1; k--) {
+				
+				for(int l = 1; l <= k; l++) {
+					System.out.print("*");
+				}
+				System.out.println();
+				
+			}
+	}
+
+}
+
+/*Output:
+*****
+****
+***
+**
+*
+*/
---
+++ src/com/patterns/Pattern4.java
@@ -0,0 +1,23 @@
+package com.patterns;
+
+public class Pattern4 {
+	public static void main(String[] args) {
+		
+		for(int i = 1; i <= 5; i++) {
+			
+			for(int j = 5; j >=i; j--) {
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
+/*Output:
+
+54321
+5432
+543
+54
+5*/
